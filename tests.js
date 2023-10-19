16a7845f0d03d3779252d402903cd5325fc473cb
Chanee Teriipaia-Molett <chanee.teriipaiamolett@gmail.com>
2023-10-18 20:04:53 -0500

    feat: add the third test and solution for intro-to-testing

--- tests.js
+++ tests.js
@@ -21,4 +21,7 @@ describe("sayHello", function (){
     it("should return a string when called", function () {
         expect(typeof sayHello()).toBe("string");
     });
+    it('should return "Hello, Jane!" when passed "Jane"', function () {
+        expect(typeof sayHello("Jane")).toBe("Hello, Jane!");
+    })
 })
